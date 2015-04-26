9ab73d696eaf15f2b4c41991cfb56b50229b2d0c
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-04-26 22:02:17 +0800

    Minor changes.

--- run-alignment.py
+++ run-alignment.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 	# simulated_datasets.append('OxfordNanopore-pbsim-observed_graphmap-2d-1k');			machine_names.append('nanopore');
 	simulated_datasets.append('OxfordNanopore-pbsim-observed_last-1d-1k');				machine_names.append('nanopore');
 	# simulated_datasets.append('OxfordNanopore-pbsim-observed_graphmap-1d-1k');			machine_names.append('nanopore');
-	# simulated_datasets.append('OxfordNanopore-pbsim-observed_marginalign-2d-1k');		machine_names.append('nanopore');
+	simulated_datasets.append('OxfordNanopore-pbsim-observed_marginalign-2d-1k');		machine_names.append('nanopore');
 
 	# simulated_datasets.append('Illumina-0k-single_end');								machine_names.append('illumina');
 	# simulated_datasets.append('OxfordNanopore-pbsim-observed_marginalign-2d-0k');		machine_names.append('nanopore');
@@ -40,9 +40,9 @@ if __name__ == "__main__":
 	genomes = [];
 	# genomes.append('neisseria_meningitidis');
 	# genomes.append('escherichia_coli');
-	genomes.append('saccharomyces_cerevisiae');
+	# genomes.append('saccharomyces_cerevisiae');
 	# genomes.append('caenorhabditis_elegans');
-	# genomes.append('hg19_v38-chr3');
+	genomes.append('hg19_v38-chr3');
 
 	num_processed_datasets = 0;
 	num_datasets = len(simulated_datasets) * len(genomes);
@@ -73,13 +73,14 @@ if __name__ == "__main__":
 			# 	wrapper_graphmap.run(reads_fasta, reference_file, 'nanopore', output_path, machine_suffix);
 			# else:
 			# wrapper_graphmap.run(reads_fasta, reference_file, 'nanopore', output_path, machine_suffix);
-			wrapper_graphmap.run(reads_fasta, reference_file, 'nanopore', output_path, 'test1-' + machine_suffix);
+			# wrapper_graphmap.run(reads_fasta, reference_file, 'nanopore', output_path, 'test1-' + machine_suffix);
+
 			# wrapper_graphmap.run(reads_fasta, reference_file, 'test', output_path, 'test-' + machine_suffix);
 			# 	wrapper_graphmap.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_names[machine_num] + '-' + machine_suffix);
 			# wrapper_lastal.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_suffix);
 			# wrapper_blasr.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_suffix);
 			# wrapper_bwamem.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_suffix);
-			# wrapper_blast.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_suffix);
+			wrapper_blast.run(reads_fasta, reference_file, machine_names[machine_num], output_path, machine_suffix);
 			###########################################
 			###########################################
 
